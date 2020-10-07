5a2e0b370df6566cadc63714f8873cfc27e150d1
Antonio Pires <tonan@globo.com>
2020-10-07 15:00:09 -0300

    ChessException and ChessPosition incomplete

---
+++ src/chess/ChessPosition.java
@@ -0,0 +1,42 @@
+package chess;
+
+import boardgame.Position;
+
+public class ChessPosition {
+
+	private char column;
+	private int row;
+	
+	public ChessPosition(char column, int row) {
+		if (column <'a' || column > 'h' || row <1 || row >8) {
+			throw new ChessException("Error instantiating ChessPosition. Valid values are from a1 to h8.");
+		}
+		this.column = column;
+		this.row = row;
+	}
+
+	public char getColumn() {
+		return column;
+	}
+
+	public int getRow() {
+		return row;
+	}
+
+	protected Position toPosition() {
+		/*matrix_row=8-chess_row  (Coluna da matriz é igual a 8 menos a coluna do xadrez - Fórmula)
+		 matrix_column=chess_column-'a' (Coluna da matriz é igual a coluna do xadrez menos caractere a - Fórmula)
+		a=0
+		b=1
+		c=2
+		'a'-'a'=0  (código do caractere a menos ele mesmo, como é o mesmo codigo, vai dar 0)
+		'b'-'a'=1
+		'c'-'a'=2 
+		*/
+		return new Position(8-row,column-'a');
+	}
+	
+	protected static ChessPosition fromPosition(Position position) {
+		return new ChessPosition()
+	}
+}
