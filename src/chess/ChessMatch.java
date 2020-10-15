7088c54192509f2762f2392550958ec3974ad88a
Antonio Pires <tonan@globo.com>
2020-10-15 14:58:16 -0300

    Promotion started not finished

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -22,6 +22,7 @@ public class ChessMatch {
 	private boolean check;
 	private boolean checkMate;
 	private ChessPiece enPassantVulnerable;
+	private ChessPiece promoted;
 
 	private List<Piece> piecesOnTheBoard = new ArrayList<>();
 	private List<Piece> capturedPieces = new ArrayList<>();
@@ -51,6 +52,10 @@ public class ChessMatch {
 
 	public ChessPiece getEnPassantVulnerable() {
 		return enPassantVulnerable;
+	}
+	
+	public ChessPiece getPromoted() {
+		return promoted;
 	}
 
 	public ChessPiece[][] getPieces() {
@@ -83,6 +88,16 @@ public class ChessMatch {
 		}
 
 		ChessPiece movedPiece = (ChessPiece) board.piece(target);
+		
+		// #specialmove promotion
+		
+		promoted=null;
+		if (movedPiece instanceof Pawn) {
+			if ((movedPiece.getColor() == Color.WHITE && target.getRow()==0 || movedPiece.getColor()==Color.BLACK && target.getRow()==7)) {
+				promoted=(ChessPiece)board.piece(target);	
+			}
+		}
+		
 
 		check = (testCheck(opponent(currentPlayer))) ? true : false;
 
